2c16742aadbdb2f39a787120865990dc74dabbce
JonathanFRocha <jonathanferreira.contato@gmail.com>
2021-07-16 19:34:37 -0300

    Created ChessPosition class

---
+++ terminal_chess/Chess/PositionChess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using board;
+
+namespace Chess
+{
+    class PositionChess
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public PositionChess(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column +  Row;
+        }
+    }
+}
--- terminal_chess/Program.cs
+++ terminal_chess/Program.cs
@@ -9,21 +9,24 @@ namespace terminal_chess
         static void Main(string[] args)
         {
 
-            try
-            {
-                Board board = new Board(8, 8);
+            PositionChess pos = new PositionChess('a', 1);
+            Console.WriteLine(pos);
 
-                board.InsertPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.InsertPiece(new King(board, Color.Black), new Position(0, 6));
-                board.InsertPiece(new Tower(board, Color.Black), new Position(1, 3));
-                board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
+            //try
+            //{
+            //    Board board = new Board(8, 8);
 
-                Screen.ShowBoard(board);
-            }
-            catch(BoardException e)
-            {
-                Console.WriteLine("Board Exception: " + e.Message);
-            }
+            //    board.InsertPiece(new Tower(board, Color.Black), new Position(0, 0));
+            //    board.InsertPiece(new King(board, Color.Black), new Position(0, 6));
+            //    board.InsertPiece(new Tower(board, Color.Black), new Position(1, 3));
+            //    board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
+
+            //    Screen.ShowBoard(board);
+            //}
+            //catch(BoardException e)
+            //{
+            //    Console.WriteLine("Board Exception: " + e.Message);
+            //}
 
             Console.ReadLine();
         }
